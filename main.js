7edde9449deb799ea07d78b316b55260e447dc9d
YuraGil <105660510+YuraGil@users.noreply.github.com>
2022-12-15 20:25:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/puh.jpg') {
-      myImage.setAttribute ('src','images/puh1.jpg');
+    if(mySrc === 'puh.jpg') {
+      myImage.setAttribute ('src','puh1.jpg');
     } else {
-      myImage.setAttribute ('src','images/puh.jpg');
+      myImage.setAttribute ('src','puh.jpg');
       
     }
 }
